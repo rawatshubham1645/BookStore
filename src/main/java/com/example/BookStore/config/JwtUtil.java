b709a618450df9ce95ad7be493e9de76edd122ef
rawatshubham1645 <rawatshubham1645@gmail.com>
2025-05-06 01:42:14 +0530

    change java version

---
+++ src/main/java/com/example/BookStore/config/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.example.BookStore.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    // Generate token with expiration
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)  // Set expiration time
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+//    public boolean validateToken(String token, UserDetails userDetails) {
+//        final String username = extractUsername(token);
+//        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+//    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ src/main/java/com/example/BookStore/service/UserDetailServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.BookStore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl  {
+}
